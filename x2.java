76db4665643f319572dddaa3134136de24798ba7
clarc54 <chris20topher@yahoo.com>
2015-09-25 12:57:27 -0400

    Update x2.java

--- x2.java
+++ x2.java
@@ -25,8 +25,41 @@ void setup() {
 
 //// NEXT FRAME:  scene, action, show.
 void draw() {
-  scene();
-  hero();
+  // SCENE:  sky, sun, tree, house, etc.
+  background( 100,150,200 );                // sky
+  fill( 255,255,0 );
+  ellipse( width*3/4, height/8, 40,40 );    // sun
+  // Grass
+  fill( 100,200,100 );
+  rect( 0,horizon, width,height*3/4 );      // grass.
+  line(mouseX, 0, mouseX, 480);             //Mouse X Coordinate
+  line(0,mouseY,640,mouseY);                //Mouse Y Coordinate
+  text(mouseX, 50,50);                      // Mouse X coordinate on screen
+  text(mouseY, 50,70);                      // Mouse Y coordinate on screen
+  /* INSERT YOUR CODE HERE! */
+  //Tree
+  fill(139,69,19);
+  rect( 150,50,50,70 );
+  fill(0,100,0);
+  triangle(135,50, 175,0, 215,50);
+  // House
+  fill(255,228,196);
+  rect(300,60, 85,60);
+  fill(75,75,75);
+  triangle(300,60, 340,20, 385,60);
+  fill(88,58,12);
+  rect(310,80, 20,40);
+  fill(255);
+  rect(350,86, 25,25);
+  //Hero
+  fill(255,0,0); 
+  ellipse(x,y, 30,50);
+  text( "Pal", x,y );
+  
+   //// ACTION:  move (x,y) coordinates.
+  x=  x + dx;
+  y=  y + dy;
+  
   dog();
   messages();
 }
@@ -59,7 +92,6 @@ void dog() {
 
 
 
-
 //////// HANDLERS:  mouse clicks, keys
 void mousePressed() {
   x=  mouseX;                             // Set (x,y) to mouse
@@ -68,13 +100,8 @@ void mousePressed() {
   dx=  random( -6, +6 );                  // random speed.
   dy=  random( -4, +4 );
 }
-
 void keyPressed() {
   if (key == 'q') {
-    exit();                           // press 'q' key to QUIT.
-  }
-  /* INSERT YOUR CODE HERE! */
+    exit(); }             // press 'q' key to QUIT.
 }
-   
-   
-
+  /* INSERT YOUR CODE HERE! */
